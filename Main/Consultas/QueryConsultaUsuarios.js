796dd59d6c1e8044d40785fe2fdde8db119a056b
cesar <cesar.rodriguezg@usantotomas.edu.co>
2023-12-01 12:49:45 -0500

    se agrega query de insersion con validacion de existencia de correo

--- Main/Consultas/QueryConsultaUsuarios.js
+++ Main/Consultas/QueryConsultaUsuarios.js
@@ -31,8 +31,44 @@ async function EliminarUsuario(id)
         throw new Error(`Se presento un Error en ${e.procName}.....${e.message}`)
     }
 }
+async function AgregarUsuario(Correo, Contrasena, Rol) {
+    try {
+      let pool = await sql.connect(cnxlocal);
+      var Respuesta={
+        Estado:"",
+    }
+      // Verificar si ya existe un usuario con el mismo correo
+      let verificarUsuario = await pool
+        .request()
+        .input('Correo', sql.VarChar(255), Correo)
+        .query('SELECT * FROM Usuarios WHERE Correo = @Correo');
+      if (verificarUsuario.recordset.length > 0) {
+        // Si ya existe un usuario con el mismo correo, puedes manejar el error o devolver algún indicativo
+        return Respuesta.Estado=false
+      }
+      else{
+        Respuesta.Estado=true;
+        // Si no existe, procede con la inserción
+        let insertarUsuario = await pool
+        .request()
+        .input('Correo', sql.VarChar(255), Correo)
+        .input('Contrasena', sql.VarChar(255), Contrasena)
+        .input('Rol', sql.VarChar(50), Rol)
+        .query('INSERT INTO Usuarios (Correo, Contrasena, Rol) VALUES (@Correo, @Contrasena, @Rol)');
+      }
+      
+
+  
+      return Respuesta;
+  
+    } catch (e) {
+      throw new Error(`Se presentó un error en ${e.procName}.....${e.message}`);
+    }
+  }
+  
 module.exports = {
     consultarUsuarios,
-    EliminarUsuario
+    EliminarUsuario,
+    AgregarUsuario
     // Otros métodos o variables que necesites exportar
 };
--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const cors = require('cors'); // Asegúrate de agregar esta línea
 const router = express.Router();
 const Login = require('./Main/Controladores/Login.js');
 const delet=require('./Main/Controladores/EliminarUsuarios.js');
+const addUsuario=require('./Main/Controladores/AgregarUsuario.js')
 // No necesitas una segunda declaración de cors aquí
 
 app.use(cors());
 
 var whitelist = process.env.PORT || ['http://localhost:3023']; // Permito que solo esta URL se conecte
 app.use(express.json());
+app.use('/add', addUsuario);
 app.use('/api', Login);
 app.use('/delete',delet);
 
