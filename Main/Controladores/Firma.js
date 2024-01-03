e125457368abf68a51d115ac1a7aa4e385edcfb2
cesar <cesar.rodriguezg@usantotomas.edu.co>
2024-01-02 21:42:09 -0500

    se agrega nuevas librerias

--- Main/Controladores/Firma.js
+++ Main/Controladores/Firma.js
@@ -5,6 +5,8 @@ const router=expres.Router();
 const jwt = require('jsonwebtoken');
 const llave="ro8BS6Hiivgzy8Xuu09JDjlNLnSLldY5p";
 const QueryFirma= require('../Consultas/QueryFirma');
+const fs = require('fs');
+const PDFDocument = require('pdfkit');
 
 const verify=(req,res,next)=>{
     ////leo la cabezera
@@ -36,6 +38,50 @@ router.post('/FirmaInsert',verify,async(request,response)=>{
     console.log(request);
     const resultadodos=await QueryFirma.InsertarFirma(request.body);
     console.log(request.body);
+    Respuesta.Estado=true;
+    response.send(Respuesta);
+
+})
+router.post('/certificadoempresa',async(request,response)=>{
+    var Respuesta={
+        Estado:"",
+    }
+    console.log(request);
+    // Simulación de la firma almacenada en la base de datos (reemplaza con tu lógica de obtención de la firma)
+    const firmaBase64 = '...'; // Tu firma almacenada en base64
+
+    // Ruta donde se guardará el PDF
+    const outputPath = 'output.pdf';
+
+    // Crear un nuevo documento PDF
+    const doc = new PDFDocument();
+
+    // Crear un flujo de escritura para guardar el PDF en el sistema de archivos
+    const stream = fs.createWriteStream(outputPath);
+
+    // Pipe el flujo de escritura al documento PDF
+    doc.pipe(stream);
+
+    // Agregar contenido al PDF
+    doc.fontSize(14).text('Este es un documento PDF con la firma:', 50, 50);
+    doc.moveDown(); // Mover hacia abajo para separar el texto y la firma
+
+    // Decodificar la firma base64 y agregarla al PDF como una imagen
+    const firmaBuffer = Buffer.from(firmaBase64, 'base64');
+    doc.image(firmaBuffer, { width: 200, height: 100, align: 'center' });
+
+    // Finalizar y cerrar el documento PDF
+    doc.end();
+
+    // Escuchar el evento 'finish' del flujo de escritura para saber cuándo se ha completado la escritura del PDF
+    stream.on('finish', () => {
+    console.log(`PDF creado exitosamente en: ${outputPath}`);
+    });
+
+    // Manejar errores
+    stream.on('error', (err) => {
+    console.error('Error al escribir el PDF:', err);
+    });
     Respuesta.Estado=true;
     response.send(Respuesta);
 
